5538f85e4fc4824b17d1def65be8d277345fb01d
Snaquaza <Snaquaza@Dragonfield.com>
2016-09-04 10:21:21 +0200

    Update formats.js

--- config/formats.js
+++ config/formats.js
@@ -409,7 +409,7 @@ exports.Formats = [
 			battle: 1,
 		},
 		mod: 'rpg',
-		team: 'randomSeasonalMelee',
+		team: 'randomRPG',
 		ruleset: ['Sleep Clause Mod', 'Freeze Clause Mod', 'HP Percentage Mod', 'Cancel Mod'],
 		onBegin: function () {
 			this.add('message', "ROCK!");
